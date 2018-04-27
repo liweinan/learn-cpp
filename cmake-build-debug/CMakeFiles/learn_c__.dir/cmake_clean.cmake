1fc1e17dba44ba69953068881447b37c17275f0a
Weinan Li <l.weinan@gmail.com>
2018-04-27 21:21:27 +0800

    .

---
+++ cmake-build-debug/CMakeFiles/learn_c__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learn_c__.dir/main.cpp.o"
+  "learn_c__.pdb"
+  "learn_c__"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learn_c__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
